54f197df4ae52a9189c60a4b4071d8f5c3d98895
Danil Ershov <danilersh0v@yandex.ru>
2025-04-23 10:42:21 +0300

    First version under VCS

---
+++ mklinks.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+ln -s ~/dotfiles/dunst ~/.config
+ln -s ~/dotfiles/hypr ~/.config
+ln -s ~/dotfiles/kitty ~/.config
+ln -s ~/dotfiles/nvim ~/.config
+ln -s ~/dotfiles/waybar ~/.config
+ln -s ~/dotfiles/wofi ~/.config
